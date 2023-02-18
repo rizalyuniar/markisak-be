bbac10721259bb83ac28244b4646ef64ca8b4aaa
breadsticks64 <alif.anke.bima@gmail.com>
2023-02-18 22:04:52 +0700

    changed expire token length

--- src/helper/auth.js
+++ src/helper/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (payload) => {
     const verifyOpts = {
-        expiresIn: "1h",
+        expiresIn: "30m",
         issuer: "markisak",
     };
     const token = jwt.sign(payload, process.env.SECRET_KEY_JWT, verifyOpts);
