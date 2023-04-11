7663c436f4e4365583e7e8f6ec8afa0624e9e168
breadsticks64 <alif.anke.bima@gmail.com>
2023-04-11 10:17:59 +0700

    cyclic migration

--- src/config/googleDrive.config.js
+++ src/config/googleDrive.config.js
@@ -14,7 +14,7 @@ const auth = new google.auth.GoogleAuth({
     "type": process.env.GOOGLE_DRIVE_TYPE,
     "project_id": process.env.GOOGLE_DRIVE_PROJECT_ID,
     "private_key_id": process.env.GOOGLE_DRIVE_PRIVATE_KEY_ID,
-    "private_key": process.env.GOOGLE_DRIVE_PRIVATE_KEY,
+    "private_key": process.env.GOOGLE_DRIVE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email": process.env.GOOGLE_DRIVE_CLIENT_EMAIL,
     "client_id": process.env.GOOGLE_DRIVE_CLIENT_ID,
     "auth_uri": process.env.GOOGLE_DRIVE_AUTH_URI,
